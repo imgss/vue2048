e99fac0333f30b393d2f210d2c03a104db57a586
imgss <838632526@qq.com>
2017-02-27 21:23:36 +0800

    游戏初始化随机生成卡片

--- src/components/gamecontainer/tiles/index.vue
+++ src/components/gamecontainer/tiles/index.vue
@@ -1,15 +1,72 @@
 <template>
-  <tile></tile>
+  <div class='tiles'>
+  <tile v-for='tile in tiles' class='tile' :tilenum="tile.number"></tile>
+  </div>
 </template>
 
 <script>
     import tile from './tile'
     export default {
+        data() {
+            return {
+                tileslength: 16,
+                tiles: []
+            }
+        },
         components: {
             tile
+        },
+        created() {
+            for (var i = 0; i < 4; i++) {
+                for (var j = 0; j < 4; j++) {
+                    this.tiles.push({
+                        number: null,
+                        x: i,
+                        y: j
+                    });
+                }
+            }
+            this.gameInit(4);
+        },
+        methods: {
+            gameInit(size) {
+                for (var i = 0; i < size; i++) {
+                    var position = Math.floor(15 * Math.random());
+                    console.log(position);
+                    this.tiles[position].number = this.randomNumber();
+                }
+            },
+            randomTile() {
+                var tile = {};
+                tile.number = this.randomNumber();
+                tile.x = this.randomPosition();
+                tile.y = this.randomPosition();
+                return tile;
+
+            },
+            randomPosition() {
+                return Math.floor(4 * Math.random());
+            },
+            randomNumber() {
+                return 2 + 2 * Math.round(Math.random());
+            }
         }
 
     }
 </script>
 
-<style></style>
+<style>
+    .tiles {
+        position: absolute;
+        width: 95%;
+        height: 95%;
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
+    }
+    
+    .tile {
+        width: 24%;
+        height: 24%;
+    }
+</style>
--- src/components/gamecontainer/tiles/tile.vue
+++ src/components/gamecontainer/tiles/tile.vue
@@ -1,11 +1,42 @@
 <template>
-    <div class='tile'></div>
+    <div class='tile' :style="{backgroundColor:colormap[tilenum]}">{{tilenum}}</div>
 </template>
 
 <script>
     export default {
+        data() {
+            return {
+                colormap: {
+                    2: "#eee4da",
+                    4: "#ede0c8",
+                    8: "#f2b179",
+                    16: '#f59563',
+                    32: '#f67c5f',
+                    64: '#f65e3b',
+                    128: '#edcf72',
+                    256: '#edcc61',
+                    512: '#edc850',
+                    1024: '#edc53f',
+                    2048: '#edc22e',
+                    4096: '#3c3a32',
+                    8192: '#3c3a32',
+                }
+            }
+        },
+        props: {
+            tilenum: null
+        }
 
     }
 </script>
 
-<style></style>
+<style>
+    .tile {
+        font-size: 3rem;
+        color: #f9f6f2;
+        text-align: center;
+        user-select: none;
+        line-height: 120px;
+        vertical-align: middle;
+    }
+</style>
